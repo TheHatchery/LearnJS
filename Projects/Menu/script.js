3c2fa563fbeeca79d09660e9c633e0200a60681c
Steve <steve@macbook-pro.home>
2022-07-06 20:41:18 +0100

    Alterntive ways

--- Projects/Menu/script.js
+++ Projects/Menu/script.js
@@ -1,6 +1,7 @@
 const topButton = document.querySelectorAll('.button');
 
-const dispState = ['none','flex'];//<--- Only required for option two
+// const dispState = ['none','flex'];//<--- Only required for option two
+let state = 0; //<--- Only required for option three
 
 topButton.forEach(element => {
     element.addEventListener('click', () => {
@@ -15,16 +16,20 @@ topButton.forEach(element => {
         //     document.querySelector(`#${element.id}A`).style.display = 'none';
         // }
 
-        //--OPTION TWO--
-        if(cssEle.display === dispState[0]){ //<--- Less code but only worth for one button at a time
-            document.querySelector(`#${element.id}A`).style.display = dispState[1];
-            [dispState[0],dispState[1]] = [dispState[1],dispState[0]];
+        // //--OPTION TWO--
+        // if(cssEle.display === dispState[0]){ //<--- Less code but only worth for one button at a time
+        //     document.querySelector(`#${element.id}A`).style.display = dispState[1];
+        //     [dispState[0],dispState[1]] = [dispState[1],dispState[0]];
+        // }
+
+        //--OPTION THREE--
+        const dispState = ['none','flex'];
+        if(cssEle.display === dispState[state]){ //<--- Less code but only worth for one button at a time
+            state = (state == 0) ? 1 : 0;
+            document.querySelector(`#${element.id}A`).style.display = dispState[state]; 
         }
-
-
     
     });
 });
 
-// topButton[0].id
 
